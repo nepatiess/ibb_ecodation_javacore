e42d59dd287372740e770095d1134573a48ecb47
Zeynep Koz <zeynepkozz52@gmail.com>
2025-02-10 22:20:00 +0300

    .

---
+++ src/main/java/com/zeynepkoz/part_1_javatypes/_01_javaPsvm.java
@@ -0,0 +1,17 @@
+package com.zeynepkoz.part_1_javatypes;
+
+// single comment
+
+/*
+Multiple comment
+ */
+public class _01_javaPsvm {
+
+    //psvm
+    public static void main(String[] args) {
+        // Çıktı
+        // sout
+        System.out.println("Hello World");
+    }
+} // end class
+
---
+++ src/main/java/com/zeynepkoz/part_1_javatypes/_02_JavaPrimitiveTypes.java
@@ -0,0 +1,37 @@
+package com.zeynepkoz.part_1_javatypes;
+
+public class _02_JavaPrimitiveTypes {
+
+    public static void main(String[] args) {
+        // Primitive type 8 tanedir, null değer alamayız.
+
+        // tam sayılar (4)
+
+        byte b1 = 127;
+        System.out.println("bytes: " + b1);
+
+        short s1 = 16516;
+        System.out.println("short: " + s1);
+
+        int i1 = 254465221;
+        System.out.println("int: " + i1);
+
+        long l1 = 9845263567787252L;
+        System.out.println("long: " + l1);
+
+        // virgüllü sayılar (floating point)
+        float f2 = 3.14f;
+        System.out.println("float: " + f2);
+
+        double d2 = 3.14;
+        System.out.println("double: " + d2);
+
+        boolean b2 = true;
+        System.out.println("boolean: " + b2);
+
+        char c2 = 'A';
+        System.out.println("char: " + c2);
+
+
+    }
+}
